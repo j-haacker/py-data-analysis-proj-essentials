08f50c99c73e7bc68fae7ac2822cdfdfb5d0bef1
jan <152862650+j-haacker@users.noreply.github.com>
2025-06-30 15:30:53 +0200

    fix initial issues

---
+++ {{cookiecutter.top_level_dir_name}}/.renew_venv.sh
@@ -0,0 +1,14 @@
+#!/usr/env/{{cookiecutter.shell}}
+source ~/.{{cookiecutter.shell}}rc
+[ -e .venv ] && yes | rm -r .venv
+if command -v {{cookiecutter.python_version_or_env}} >/dev/null 2>&1
+then
+    {{cookiecutter.python_version_or_env}}  -m venv .venv
+else
+    mamba activate {{cookiecutter.python_version_or_env}}
+    python -m venv .venv
+    mamba deactivate
+    mamba deactivate  # make sure to also quit "base"
+fi
+source .venv/bin/activate
+pip install -e ".[all]"
